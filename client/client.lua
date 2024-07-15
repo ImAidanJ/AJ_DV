d606e6b603c71912e6a046c16820e68bf546df4e
ImAidanJ <aidanjhamblin@gmail.com>
2024-07-14 23:46:23 -0400

    fixed errors. works as intended :)

--- client/client.lua
+++ client/client.lua
@@ -21,16 +21,22 @@ AddEventHandler('AJ:DV', function()
             if (GetPedInVehicleSeat(vehicle, -1) == playerPed) then 
                 DV(vehicle, 5)
             else
-                TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"Must be in Driver Seat to Delete Vehicle."}})
+                TriggerEvent("chat:addMessage", {
+                    template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', 
+                    args = {"Must be in Driver Seat to Delete Vehicle."}
+                })
             end
         else
-            local playerLookingAt GetOffsetFromEntityInWorldCoords(playerPed, 0.0, 5.0, 0.0)
+            local playerLookingAt = GetOffsetFromEntityInWorldCoords(playerPed, 0.0, 5.0, 0.0)
             local vehicle = VehicleInDirection(playerPed, coords, playerLookingAt)
 
             if (DoesEntityExist(vehicle)) then 
                 DV(vehicle, 5)
             else
-                TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"Must be in or near Vehicle to Delete Vehicle."}})
+                TriggerEvent("chat:addMessage", {
+                    template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', 
+                    args = {"Must be in or near Vehicle to Delete Vehicle."}
+                })
             end
         end
     end
@@ -38,7 +44,7 @@ end)
 
 -- Register Command --
 
-RegisterCommand('dv' function()
+RegisterCommand('dv', function()
     TriggerEvent('AJ:DV')
 end, false)
 TriggerEvent( "chat:addSuggestion", "/dv", "deletes vehicle your in or around." )
@@ -52,24 +58,36 @@ function DV(vehicle, timeoutMax)
     DeleteVehicle(vehicle)
 
     if (DoesEntityExist(vehicle)) then
-        TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"Failed to Delete Vehicle.. Trying Again."}})
+        TriggerEvent("chat:addMessage", {
+            template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', 
+            args = {"Failed to Delete Vehicle.. Trying Again."}
+        })
 
         while (DoesEntityExist(vehicle) and timeout < timeoutMax) do
             DeleteVehicle(vehicle)
 
             if (not DoesEntityExist(vehicle)) then
-                TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(46, 235, 94, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"Vehicle Deleted Successfully."}})
+                TriggerEvent("chat:addMessage", {
+                    template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(0, 255, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', 
+                    args = {"Vehicle Deleted Successfully."}
+                })
             end
 
             timeout = timeout + 1
             Citizen.Wait(500)
 
             if (DoesEntityExist(vehicle) and (timeout == timeoutMax - 1)) then
-                TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"Failed to delete vehicle after " .. timeoutMax .. " retries."}})
+                TriggerEvent("chat:addMessage", {
+                    template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(255, 0, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', 
+                    args = {"Failed to delete vehicle after " .. timeoutMax .. " retries."}
+                })
             end
         end
     else
-        TriggerClientEvent("chat:addMessage", -1, { template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(46, 235, 94, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', args = {"Vehicle Deleted Successfully."}})
+        TriggerEvent("chat:addMessage", {
+            template = '<div style="padding: 0.5vw; text-align: center; margin: 0.5vw; background-color: rgba(0, 255, 0, 0.6); border-radius: 3px; color: white;"><b>{0}</b></div>', 
+            args = {"Vehicle Deleted Successfully."}
+        })
     end
 end
 
